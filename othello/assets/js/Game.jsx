d2afc2d462ffb1aed76c453987157cd971c4e458
Wycode-fish <wangyichen0427@gmail.com>
2018-03-07 18:20:18 -0500

    score board

--- othello/assets/js/Game.jsx
+++ othello/assets/js/Game.jsx
@@ -7,14 +7,14 @@ import Menu from './Menu'
 import { searchPos, nextAvailables, nextBoard, list2Arr, getScore, arr2List } from './logic'
 
 // Global Constants
-let TOP_LEFT 	= 1;
-let TOP 	 	= 2;
-let TOP_RIGHT 	= 3;
-let RIGHT 		= 4;
-let DOWN_RIGHT 	= 5;
-let DOWN 		= 6;
-let DOWN_LEFT 	= 7;
-let LEFT 		= 8;
+let TOP_LEFT  = 1;
+let TOP    = 2;
+let TOP_RIGHT  = 3;
+let RIGHT   = 4;
+let DOWN_RIGHT  = 5;
+let DOWN   = 6;
+let DOWN_LEFT  = 7;
+let LEFT   = 8;
 let SIZE = 8;
 
 export default function run_game(root, channel) {
@@ -33,7 +33,8 @@ class Game extends Component {
       blackScore: 0,
       whiteScore: 0,
       player1: "",
-      player2: ""
+      player2: "",
+      opaque: -1,
     }
     this.channel.join()
          .receive("ok", this.gotView.bind(this))
@@ -58,7 +59,8 @@ class Game extends Component {
         <Row>
           <Col lg="8">{this.renderTiles(this.state.tiles)}</Col>
           <Col lg="4">
-            <Menu blackScore={this.state.blackScore} whiteScore={this.state.whiteScore}/>
+            <Menu current={this.state.current} player1={this.state.player1} player2={this.state.player2}
+               blackScore={this.state.blackScore} whiteScore={this.state.whiteScore}/>
           </Col>
         </Row>
       </Container>
@@ -73,7 +75,10 @@ class Game extends Component {
         {_.map(tiles, (tile,index) =>
           <Tile key = {index} index={index} content={tile} availables={this.state.availables}
             current={this.state.current}
-            clickTile={this.clickTile.bind(this)} />
+            opaque={this.state.opaque}
+            clickTile={this.clickTile.bind(this)}
+            onEnterChange={this.onEnterChange.bind(this)}
+            onLeaveChange={this.onLeaveChange.bind(this)} />
         )}
       </Row>
       </div>
@@ -141,5 +146,12 @@ class Game extends Component {
       if (a[0] == x && a[1] == y) flag = true;
     })
     return flag;
+  }
+
+  onEnterChange(index) {
+    this.setState({opaque: index});
+  }
+  onLeaveChange(index) {
+    this.setState({opaque: -1});
   }
 }
--- othello/assets/js/Menu.jsx
+++ othello/assets/js/Menu.jsx
@@ -1,10 +1,15 @@
 import React, { Component } from 'react';
 
 export default function(props) {
+  const turn = () => {
+    console.log(props.player1)
+    return props.current == 1 ? props.player1 + "'s Turn" : props.player2 + "'s Turn"
+  }
   return (
-    <div>
-      <p>White: {props.whiteScore}</p>
-      <p>Black: {props.blackScore}</p>
+    <div className="scores">
+      <p>White: {props.whiteScore}</p >
+      <p>Black: {props.blackScore}</p >
+      <p>{turn()}</p >
     </div>
   )
 }
